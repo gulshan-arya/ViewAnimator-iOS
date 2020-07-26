30c24c1930f7e9d6ba560c88232a0ba8094dc152
Gulshan Kumar <singh.aryan7575@gmail.com>
2020-07-26 22:21:54 +0530

    initial project setup

---
+++ ViewAnimator/ViewAnimator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewAnimator
+//
+//  Created by Gulshan on 26/07/20.
+//  Copyright © 2020 Gulshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
