d6a080c5cdf9427e396dfa3efa2db6bfcd9625ca
Gulshan Kumar <singh.aryan7575@gmail.com>
2020-07-26 23:08:21 +0530

    adding support of swift 4.2

--- ViewAnimato-iOS.podspec
+++ ViewAnimato-iOS.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name         = "ViewAnimato-iOS"
-  spec.version      = "0.0.1"
+  spec.version      = "0.0.3"
   spec.summary      = "ViewAnimator is a library for building complex iOS UIView animations in an easy way"
   spec.description  = <<-DESC
                     PhotoFeeds provides you with smooth loading of images from the server in a MVVM architecture. It uses a third party library to fetch images!
@@ -11,6 +11,6 @@ Pod::Spec.new do |spec|
   spec.platform     = :ios, "13.0"
   spec.source       = { :git => "https://github.com/gulshan-arya/ViewAnimator-iOS.git", :tag => "#{spec.version}" }
   spec.source_files = "ViewAnimator/ViewAnimator/*.{swift}"
-  spec.swift_version = "4.0"
- # spec.swift_versions = ["4.0"]
+  spec.swift_version = "4.2"
+  spec.swift_versions = ["4.0", "4.2"]
 end
--- ViewAnimator/ViewAnimator/ViewAnimator.swift
+++ ViewAnimator/ViewAnimator/ViewAnimator.swift
@@ -8,61 +8,6 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
-
-    private var items = [Any?]()
-    private let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
-    private let animations = [AnimationType.from(direction: .bottom, offset: 30.0)]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupActivityIndicator()
-    }
-
-    private func setupActivityIndicator() {
-        activityIndicator.center = CGPoint(x: view.center.x, y: 100.0)
-        activityIndicator.hidesWhenStopped = true
-        view.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
-        cell.textLabel?.text = "Cell: \(indexPath.row + 1)"
-        return cell
-    }
-
-    @IBAction func animateTapped(_ sender: UIBarButtonItem) {
-        sender.isEnabled = false
-        activityIndicator.stopAnimating()
-        items = Array(repeating: nil, count: 20)
-        tableView.reloadData()
-        UIView.animate(views: tableView.visibleCells, animations: animations, completion: {
-            sender.isEnabled = true
-        })
-    }
-
-    @IBAction func resetTapped(_ sender: UIBarButtonItem) {
-        items.removeAll()
-        UIView.animate(views: tableView.visibleCells, animations: animations, reversed: true,
-                       initialAlpha: 1.0, finalAlpha: 0.0, completion: {
-            self.tableView.reloadData()
-            self.activityIndicator.startAnimating()
-        })
-    }
-}
-
-
 // MARK: - UIView extension with animations.
 public extension UIView {
 
@@ -82,7 +27,7 @@ public extension UIView {
                         finalAlpha: CGFloat = 1.0,
                         delay: Double = 0,
                         duration: TimeInterval = ViewAnimatorConfig.duration,
-                        options: UIViewAnimationOptions = [],
+                        options: UIView.AnimationOptions = [],
                         completion: (() -> Void)? = nil) {
         
         let transformFrom = transform
@@ -121,7 +66,7 @@ public extension UIView {
                                delay: Double = 0,
                                animationInterval: TimeInterval = 0.05,
                                duration: TimeInterval = ViewAnimatorConfig.duration,
-                               options: UIViewAnimationOptions = [],
+                               options: UIView.AnimationOptions = [],
                                completion: (() -> Void)? = nil) {
 
         guard views.count > 0 else {
